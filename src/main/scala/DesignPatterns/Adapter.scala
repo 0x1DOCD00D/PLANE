03873662e2e4872ed86bfe7329366d305049e68d
0x1DOCD00D <drmark@uic.edu>
2020-06-11 12:25:31 -0500

    Added the code to show the use of the pattern Adapter

---
+++ src/main/scala/DesignPatterns/Adapter.scala
@@ -0,0 +1,67 @@
+/*
+ *
+ *  * Copyright (c) 2020 Mark Grechanik. All rights reserved.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+package DesignPatterns
+
+//the pattern Adapter is used to change the interface of some Adaptee
+object AdapterPattern extends App {
+
+  //the client makes calls to methods NewMethod1 and NewMethod2 that take no parameters
+  //however, the class Adaptee does not provide these methods and other clients keep
+  //invoking the method SomeCoreMethod
+  class Adaptee {
+    def SomeCoreMethod(msg: String) = println(msg)
+  }
+
+  //this is one way to present an adapter as a trait that obtains access
+  //to the method SomeCoreMethod
+  trait Adapter {
+    this: Adaptee =>
+    def NewMethod1 = SomeCoreMethod(this.getClass.getName)
+
+    def NewMethod2 = SomeCoreMethod(this.getClass.getPackageName)
+  }
+
+  //then we mix the trait with the adaptee and voila! - everything is ready
+  val objectWithAdaptee = new Adaptee with Adapter
+  objectWithAdaptee.NewMethod1
+  //but the original method is still available
+  objectWithAdaptee.SomeCoreMethod("bummer!")
+
+  //the other way to use an adapter is to use it as a new interface
+  trait AdapterAbstract {
+    def NewMethod1
+
+    def NewMethod2
+  }
+
+  //whereby a class implements this new interface and uses the class Adaptee internally
+  //to enable the functionality of the new interface
+  class Adapted extends AdapterAbstract {
+    private val adapteeObject = new Adaptee
+
+    override def NewMethod1: Unit = adapteeObject.SomeCoreMethod(this.getClass.getName)
+
+    override def NewMethod2: Unit = adapteeObject.SomeCoreMethod(this.getClass.getPackageName)
+  }
+
+  (new Adapted).NewMethod1
+
+  //finally, the adapter conversion can be done implicitly
+  implicit class AutomaticAdapter(adaptee: Adaptee) {
+    def NewMethod1 = adaptee.SomeCoreMethod(this.getClass.getName)
+
+    def NewMethod2 = adaptee.SomeCoreMethod(this.getClass.getPackageName)
+  }
+
+  //AutomaticAdapter((new Adaptee)).NewMethod
+  (new Adaptee).NewMethod1
+  (new Adaptee).NewMethod2
+}
