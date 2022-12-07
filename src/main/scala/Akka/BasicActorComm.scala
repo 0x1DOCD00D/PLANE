9b12bc58bf3d4523ddc1bbf9a94419fc3684785d
Mark Grechanik <markgrechanik@gmail.com>
2022-12-07 13:05:18 -0600

    implemented a basic untyped communications between prof and student actors.

--- src/main/scala/Akka/BasicActorComm.scala
+++ src/main/scala/Akka/BasicActorComm.scala
@@ -1,5 +1,45 @@
+/*
+ * Copyright (c) 2022 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package Akka
 
-object BasicActorComm {
+import akka.actor.{Actor, ActorSystem, Props, ActorRef}
 
-}
+object BasicActorComm:
+  val actorSystem: ActorSystem = ActorSystem("actOnIt1")
+
+  case class InformStudentAboutProfessor(professor: ActorRef)
+
+  class Professor(name: String) extends Actor:
+    override def receive: Receive =
+      case question: String =>
+        println(s"[question: ] $question from ${sender().toString()}")
+        sender() ! "There is never enough space for anything!"
+      case _ => println("[Professor received unknown message]")
+
+  class Student(name: String) extends Actor:
+    override def receive: Receive =
+      case InformStudentAboutProfessor(prof) => prof ! "What is the meaning of the Universe?"
+      case answer: String => println (s"[answer from: ] ${sender().toString()} - $answer")
+      case _ => println("[Student received unknown message]")
+
+  object Student:
+    def apply(studentName: String):Props = Props(new Student(studentName))
+
+  object Professor:
+    def apply(profName: String): Props = Props(new Professor(profName))
+
+  @main def runBasicActorComm(args: String*): Unit =
+    println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/Akka/BasicActorComm.scala created at time 9:38 AM")
+    println(actorSystem.name + " is created at " + actorSystem.startTime)
+    val professor = actorSystem.actorOf(Professor("Mark"))
+    val student = actorSystem.actorOf(Student("Bumblebee"))
+    student ! 1//should print out [Student received unknown message]
+    student ! InformStudentAboutProfessor(professor)
+    Thread.sleep(5000)
+    actorSystem.terminate()
--- src/main/scala/CatsIO/Http4sServer.scala
+++ src/main/scala/CatsIO/Http4sServer.scala
@@ -1,5 +1,13 @@
 package CatsIO
-
+import cats.effect._
+import cats.syntax.all._
+import org.http4s._, org.http4s.dsl.io._, org.http4s.implicits._
 object Http4sServer {
+  val service = HttpRoutes.of[IO] {
+    case _ =>
+      IO(Response(Status.Ok))
+  }
 
+  @main def runhttp4s(): Unit =
+    println("hi")
 }
