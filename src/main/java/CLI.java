122281b72a6b7823518f151b11b2395166b367b9
Mark Grechanik <markgrechanik@gmail.com>
2023-01-02 19:03:46 -0600

    modifications to cli and simple thread count without synchronization

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 import java.util.Scanner;
 
 public class CLI {
@@ -5,7 +13,25 @@ public class CLI {
         System.out.print("Enter an integer, preferrably " + allowed + ": ");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt() == allowed;
+    }
 
+    private int getCLINumber(Scanner sc) {
+        try {
+            if(sc != null) {
+                sc.nextInt();
+            } else {
+                System.out.println("Scanner is null");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input, try again.");
+        }
+        return getCLINumber(sc);
+    }
+    public int getCLI() {
+        System.out.print("Enter an integer: ");
+        Scanner sc = new Scanner(System.in);
+        return getCLINumber(sc);
     }
     public static void main(String[] args) {
         System.out.println(new CLI().getCLI(1));
--- src/main/scala/Akka/HttpBasicService.scala
+++ src/main/scala/Akka/HttpBasicService.scala
@@ -1,5 +1,39 @@
+/*
+ * Copyright (c) 2023 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package Akka
 
-object HttpBasicService {
+import akka.actor
+import akka.actor.ActorSystem
+//import akka.actor.typed.ActorSystem
+//import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.Directives.*
 
-}
+import scala.concurrent.ExecutionContext
+import scala.io.StdIn
+
+object HttpBasicService:
+  @main def runHttpBasicService(args: String*): Unit =
+    println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/Akka/HttpBasicService.scala created at time 2:14 PM")
+    given ActorSystem = ActorSystem("HttpBasicService")
+    given ExecutionContext = summon[ActorSystem].dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    println(s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture.flatMap(_.unbind()).onComplete(_ => summon[ActorSystem].terminate())
