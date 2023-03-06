cb449206eee23053440a39f1034a062f4b9fc0bd
Mark Grechanik <markgrechanik@gmail.com>
2023-03-05 20:06:48 -0600

    updated discovery for the chitchat actor and an update for threadid

--- src/main/scala/Akka/ChitActor.scala
+++ src/main/scala/Akka/ChitActor.scala
@@ -8,15 +8,22 @@
 
 package Akka
 
-import akka.actor.{Actor, ActorLogging, PoisonPill, Props}
-import ChitChatMessages.*
+import akka.actor.{Actor, ActorIdentity, ActorLogging, Identify, PoisonPill, Props}
 
 class ChitActor extends Actor with ActorLogging {
+  //  [akka://RemoteActorSystem@192.168.1.8:25521] with UID [6678455371721108346] MDC: {akkaAddress=akka://RemoteActorSystem@192.168.1.8:25521, akkaUid=6678455371721108346, sourceThread=main, akkaSource=ArteryTransport(akka://RemoteActorSystem), sourceActorSystem=RemoteActorSystem, akkaTimestamp=20:00:29.328UTC}
+  override def preStart(): Unit = {
+    val selection = context.actorSelection("RemoteActorSystem@192.168.1.8:25521/user/chatActor")
+    selection ! Identify("DrMark")
+  }
+
   override def receive: Receive = {
     case m if m.isInstanceOf[StartChitChat.type ]  => log.info(s"Received $m from ${sender()}")
     case m if m.isInstanceOf[StopChitChat.type ]  => log.info(s"Received $m from ${sender()}")
       self ! PoisonPill
       context.system.terminate()
+    case ActorIdentity(id, Some(actorRef)) =>
+      actorRef ! s"Thank you for identifying yourself, $id!"
     case m => log.info(s"Received $m from ${sender()}")
 
   }
