285cbe7dcb7271d57407edc4a92cff6c93e2452d
0x1DOCD00D <drmark@uic.edu>
2023-04-04 14:06:50 -0500

    enum max ordinal computation

--- src/main/scala/Language/EnumExperiments.scala
+++ src/main/scala/Language/EnumExperiments.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package Language
 
 import scala.util.{Failure, Success, Try}
@@ -18,6 +26,8 @@ object EnumExperiments:
     case Failure(e) => e.getMessage
   }
 
-  @main def runIt = println(b)
-
-  println(c)
+  @main def runIt =
+    ConsistencyModels.values.foreach(v=>println(v.ordinal))
+    println("max value " + ConsistencyModels.values.map(_.ordinal).toList.max)
+    println(b)
+    println(c)
