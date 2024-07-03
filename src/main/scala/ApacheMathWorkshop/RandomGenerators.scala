99d57e864cd39ae3979878fa4272581d099f7070
Mark Grechanik <markgrechanik@gmail.com>
2024-07-03 12:49:38 -0500

    added autoloading of the distribution class and generating a sample

--- src/main/scala/ApacheMathWorkshop/RandomGenerators.scala
+++ src/main/scala/ApacheMathWorkshop/RandomGenerators.scala
@@ -12,8 +12,32 @@ import com.google.common.reflect.ClassPath
 
 import java.util.stream.Collectors
 import scala.jdk.CollectionConverters.*
+import org.apache.commons.math3.distribution.*
+import org.apache.commons.math3.linear.{MatrixUtils, RealMatrix}
+import org.apache.commons.math3.random.*
 
 object RandomGenerators:
+  def getDistribution(
+     distName: String,
+     randomGenerator: RandomGenerator
+  ): AbstractRealDistribution =
+    val pkgName = "org.apache.commons.math3.distribution"
+    val clzz = ClassPath
+      .from(ClassLoader.getSystemClassLoader)
+      .getAllClasses
+      .asScala
+      .toList
+      .filter(c => c.getPackageName.equalsIgnoreCase(pkgName))
+      .filter(c => c.getSimpleName.equalsIgnoreCase(distName))
+      .map(c => c.load())
+
+    clzz.foreach(c => println(s"Class: ${c.getName}"))
+    val distClass = clzz.head
+    distClass
+      .getDeclaredConstructor(classOf[RandomGenerator], classOf[Double], classOf[Double])
+      .newInstance(randomGenerator, 10.0, 3.0)
+      .asInstanceOf[AbstractRealDistribution]
+
   @main def runRandomGenerators(args: String*): Unit =
     val randomGeneratorClass = "Well1024a"
     val pkgName = "org.apache.commons.math3.random"
@@ -30,6 +54,10 @@ object RandomGenerators:
     val instances = clzz.map(c => c.getDeclaredConstructor().newInstance())
     instances.foreach(i => println(s"Instance: ${i.getClass.getName}"))
     instances.map(i => i.getClass.getDeclaredMethods.map(m => println(s"Method: ${m.getName}")))
+    val dist = getDistribution("NormalDistribution", instances.head.asInstanceOf[RandomGenerator])
+    val sample = dist.sample(10)
+    println(s"Sample: ${sample.mkString(", ")}")
+
     println(
        "File /Users/drmark/IdeaProjects/PLANE/src/main/scala/ApacheMathWorkshop/RandomGenerators.scala created at time 1:32PM"
     )
