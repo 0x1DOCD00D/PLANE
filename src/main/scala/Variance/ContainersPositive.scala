5dc8f6403496476090dfbe1f27fe33ee00be8f59
Mark Grechanik <markgrechanik@gmail.com>
2024-10-08 09:54:13 -0500

    covariance with cats and dogs

---
+++ src/main/scala/Variance/ContainersPositive.scala
@@ -0,0 +1,34 @@
+
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2024 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////
+
+package Variance
+
+object ContainersPositive:
+  // Defining a simple class hierarchy
+  class Animal {
+    def makeSound(): Unit = println("Some animal sound")
+  }
+
+  class Dog extends Animal {
+    override def makeSound(): Unit = println("Bark")
+  }
+
+  class Cat extends Animal {
+    override def makeSound(): Unit = println("Meow")
+  }
+
+  // Covariant Container class
+  class Container[+A](val value: A) {
+    def getValue: A = value
+  }
+
+  val dogContainer: Container[Dog] = new Container(new Dog)
+  val animalContainer: Container[Animal] = dogContainer // This works because of covariance
+
+  animalContainer.getValue.makeSound() // Will output "Bark"
+  @main def runContainersPositive(args: String*): Unit =
+    println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/Variance/ContainersPositive.scala created at time 9:52AM")
