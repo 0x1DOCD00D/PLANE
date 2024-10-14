ad64afffd2c9da9d61ff8d7dd0480bcad0d221b7
0x1DOCD00D <markgrechanik@gmail.com>
2024-10-14 18:21:34 -0500

    block code rewrite by ast manipulations

--- src/main/scala/AST/AstFromCodeSnippet.scala
+++ src/main/scala/AST/AstFromCodeSnippet.scala
@@ -1,13 +1,16 @@
 package AST
 
-import dotty.tools.dotc._
-import dotty.tools.dotc.core.Contexts._
-import dotty.tools.dotc.core.Phases._
-import dotty.tools.dotc.ast.tpd._
+import dotty.tools.dotc.*
+import dotty.tools.dotc.core.Contexts.*
+import dotty.tools.dotc.core.Phases.*
+import dotty.tools.dotc.ast.tpd.*
+import dotty.tools.dotc.ast.untpd
+import dotty.tools.dotc.core.Constants.Constant
 import dotty.tools.dotc.util.SourceFile
 import dotty.tools.io.VirtualFile
 import dotty.tools.dotc.parsing.Parsers.Parser
 import dotty.tools.dotc.reporting.Reporter
+import dotty.tools.dotc.typer.Typer
 
 object AstFromCodeSnippet {
 
@@ -63,17 +66,54 @@ object AstFromCodeSnippet {
     }
   }
 
+  def rewriteTree(tree: untpd.Tree)(implicit ctx: Context): untpd.Tree = {
+    tree match {
+      case n if n.getClass.getSimpleName == "Number" =>
+        println(s"Rewriting Number from ${n.toString} to 42")
+        untpd.Literal(Constant(42))
+
+      // Match and rewrite blocks (sequences of expressions/statements)
+      case b: untpd.Block =>
+        println("Rewriting Block")
+        val rewrittenStats = b.stats.map(rewriteTree)  // Traverse all block statements
+        val rewrittenExpr = rewriteTree(b.expr)        // Traverse the final expression
+        untpd.Block(rewrittenStats, rewrittenExpr)
+
+      // Match and rewrite ValDef nodes (variable definitions)
+      case v: untpd.ValDef =>
+        println(s"Rewriting ValDef for ${v.name}")
+        val rewrittenRhs = rewriteTree(v.rhs)          // Traverse and rewrite the RHS
+        untpd.ValDef(v.name, v.tpt, rewrittenRhs)             // Return a new ValDef
+
+      // Match and rewrite Apply nodes (method calls, like x + y)
+      case a: untpd.Apply =>
+        println("Rewriting Apply")
+        val rewrittenFun = rewriteTree(a.fun)          // Traverse and rewrite the function part
+        val rewrittenArgs = a.args.map(rewriteTree)    // Traverse and rewrite the arguments
+        untpd.Apply(rewrittenFun, rewrittenArgs)              // Return a new Apply node
+
+      // Match and rewrite Ident nodes (identifiers, like variable names x or y)
+      case i: untpd.Ident =>
+        println(s"Ident: ${i.name}")
+        i  // Leave the identifier unchanged
+
+      // Match and recursively rewrite any other tree nodes
+      case other =>
+        println(s"Other node: ${other.getClass.getSimpleName}")
+        other
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     // Initialize a basic context with default settings
     implicit val ctx: Context = (new ContextBase).initialCtx.fresh
 
     // Block of Scala code we want to parse
-    val code =
-      """
-        val x = 10
-        val y = 20
-        x + y
-      """
+    val code = """
+      val x = 10
+      val y = 20
+      x + y
+    """
 
     // Parse the code and get the AST
     val tree = getASTFromCode(code)
@@ -82,5 +122,7 @@ object AstFromCodeSnippet {
     println(s"AST of the code block:\n${tree.show}")
 
     traverseTree(tree)
+    val newTree = rewriteTree(tree)
+    println(s"AST of the NEW code block:\n${newTree.show}")
   }
 }
