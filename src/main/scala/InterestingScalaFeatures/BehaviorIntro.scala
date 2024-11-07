ba3abe01ccd9db352acdb10c654eb9497891a629
0x1DOCD00D <markgrechanik@gmail.com>
2024-11-06 19:52:45 -0600

    refinements

---
+++ src/main/scala/InterestingScalaFeatures/BehaviorIntro.scala
@@ -0,0 +1,26 @@
+package InterestingScalaFeatures
+
+object BehaviorIntro:
+  import scala.reflect.Selectable.reflectiveSelectable
+  
+  trait Animal {
+    def sound(): String
+  }
+
+  class Dog extends Animal {
+    def sound(): String = "Bark"
+  }
+
+  type PetAnimal = Animal {
+    def pet(): String
+  }
+
+  val petDog: PetAnimal = new Animal {
+    def sound(): String = "Bark"
+
+    def pet(): String = "Happy Dog"
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(petDog.pet())
+  }
