9346c885eb142cbeba581cff9533645d5c5ab6d7
0x1DOCD00D <markgrechanik@gmail.com>
2024-11-09 17:05:36 -0600

    contravariant issues with existential types

--- src/main/scala/TypeFP/ContravariantExistType.scala
+++ src/main/scala/TypeFP/ContravariantExistType.scala
@@ -3,14 +3,23 @@ package TypeFP
 import Variance.AnimalKingdom.*
 
 object ContravariantExistType:
-  // Define a contravariant consumer class
-  class Consumer[-A] {
-    def consume(value: A): Unit = println(s"Consumed: $value")
+  
+  class Consumer[-A <: Mammal] {
+    private var storage: Option[Animal] = None
+    
+    def consume(value: Animal): Unit =
+      storage = Option(value)  
+      println(s"Consumed: $value")
+    
+    def produce(): Mammal = storage.getOrElse(throw Exception("nothing stored"))
   }
 
   // Define a method that accepts a consumer of some unknown type T
   def useConsumer(c: Consumer[? >: Dog]): Unit = {
     c.consume(new Dog) 
+    c.consume(new Cat) 
+    c.consume(new SmallDog) 
+    c.consume(new Animal) 
   }
   
   def main(args: Array[String]): Unit = {
@@ -18,4 +27,5 @@ object ContravariantExistType:
     useConsumer(anyConsumer) // This works because Consumer is contravariant
     useConsumer(new Consumer[Animal]) // This works because Consumer is contravariant
     //  useConsumer(new Consumer[SmallDog]) // This does not work
+   println(anyConsumer.produce()) 
   }
