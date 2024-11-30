b95a4445662779d478df23297f922c2427865a8c
Mark Grechanik <markgrechanik@gmail.com>
2024-11-29 19:50:59 -0600

    traverse, sequence and scalaFx basics

--- src/main/scala/FPIntro/SequenceFunction.scala
+++ src/main/scala/FPIntro/SequenceFunction.scala
@@ -9,10 +9,20 @@ package FPIntro
 
 object SequenceFunction:
   def sequence[A](as: List[Option[A]]): Option[List[A]] =
-    if as.filter(_.isEmpty).length == as.length then None
+    if as.count(_.isEmpty) == as.length then None
     else Some(as.filter(_.isDefined).map(_.get))
+
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = sequence(as.map(f(_)))
     
   @main def runSequenceFunction(args: String*): Unit =
     println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/FPIntro/SequenceFunction.scala created at time 2:07PM")
-    val lst = List(None, Option("a"), None, None, Option(2.3f), Option((1, "b")))
+    val lst = List(None, Option("a"), None, None, Option(2.3f), Option((1, "b")), Option(5))
+    val lst1 = List(None, "a", None, None, 2.3f, Option((1, "b")), 5)
     println(sequence(lst))
+    val f: Any => Option[Int] = (s:Any)=>{
+      try
+        Some(s.toString.toInt)
+      catch
+        case  _ => None
+    } 
+    println(traverse(lst1)(f))
