ca804fddc2b3f2829c7276383a951907ff71722f
Mark Grechanik <markgrechanik@gmail.com>
2025-02-15 20:03:44 -0600

    cosmetic changes

--- src/main/scala/scala3Features/PatternMatchFunction.scala
+++ src/main/scala/scala3Features/PatternMatchFunction.scala
@@ -1,4 +1,3 @@
-
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2025 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -17,12 +16,10 @@ object PatternMatchFunction:
     val f0 = a => f(using a)
     inline f0 match
       case g: (A => Future[?]) => g(x).value.get.get
-      case _ => f0(x)
+      case _                   => f0(x)
 
-  
   @main def runPatternMatchFunction(args: String*): Unit =
     println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/scala3Features/PatternMatchFunction.scala created at time 5:51PM")
     val res1 = eval(Future.successful(summon[Int]), 10)
     val res2 = eval(summon[Int] + 1, 10)
     println(s"$res1, $res2")
-
