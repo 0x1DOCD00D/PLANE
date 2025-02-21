5c9d8f6d71e0c1c50cf5ce658be091f6467be005
Mark Grechanik <markgrechanik@gmail.com>
2025-02-20 20:52:16 -0600

    braces from a single import removed

--- src/main/scala/RottenGreenTests/ComplexTypeComparisonConstraint.scala
+++ src/main/scala/RottenGreenTests/ComplexTypeComparisonConstraint.scala
@@ -1,4 +1,3 @@
-
 ////////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2025 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -8,23 +7,23 @@
 package RottenGreenTests
 
 object ComplexTypeComparisonConstraint:
-  import scala.compiletime.ops.int.{+}
+  import scala.compiletime.ops.int.+
   import scala.compiletime.summonInline
 
   // 1) A sealed ADT for your expressions:
   sealed trait MyExpr
   sealed trait MySum[A <: Int, B <: Int] extends MyExpr
-  sealed trait MyConstVal[A <: Int]      extends MyExpr
+  sealed trait MyConstVal[A <: Int] extends MyExpr
 
   // 2) Convenience type aliases:
   type +++[A <: Int, B <: Int] = MySum[A, B]
-  type K[A <: Int]             = MyConstVal[A]
+  type K[A <: Int] = MyConstVal[A]
 
   // 3) A typeclass controlling which pairs *may* be compared:
   trait Allowed[E1 <: MyExpr, E2 <: MyExpr]
 
   //   - Summation vs. Const => Allowed
-  given sumConst[A <: Int, B <: Int, C <: Int]: Allowed[MySum[A,B], MyConstVal[C]] with {}
+  given sumConst[A <: Int, B <: Int, C <: Int]: Allowed[MySum[A, B], MyConstVal[C]] with {}
 
   //   - Const vs. Const => Allowed
   given constConst[A <: Int, B <: Int]: Allowed[MyConstVal[A], MyConstVal[B]] with {}
@@ -35,8 +34,8 @@ object ComplexTypeComparisonConstraint:
   //    Summation[A,B] => A + B
   //    MyConstVal[X] => X
   type Evaluate[E <: MyExpr] <: Int = E match
-    case MySum[a, b]    => a + b
-    case MyConstVal[x]  => x
+    case MySum[a, b]   => a + b
+    case MyConstVal[x] => x
 
   // 5) Compare two expressions *by their final integer values*.
   //    We unify Evaluate[E1] and Evaluate[E2], not E1 and E2 themselves.
@@ -49,7 +48,7 @@ object ComplexTypeComparisonConstraint:
     // Summation(1,3) => Evaluate => 4
     // MyConstVal(4) => Evaluate => 4
     // => unify => compile-time success
-    val ev1 = compareByValue[+++[1,3], K[4]]
+    val ev1 = compareByValue[+++[1, 3], K[4]]
     println("compareByValue[+++[1,3], K[4]] => " + ev1)
 
     // Constant vs Constant => Allowed
