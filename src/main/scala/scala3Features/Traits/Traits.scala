d22e7878c09adad7fb1680d9988cbe0bfca9efd6
Mark Grechanik <markgrechanik@gmail.com>
2025-04-09 12:53:47 -0500

    created Traits package, modify a trait inherited from a class

--- src/main/scala/scala3Features/Traits/Traits.scala
+++ src/main/scala/scala3Features/Traits/Traits.scala
@@ -5,17 +5,18 @@
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////
 
-package scala3Features
+package scala3Features.Traits
 
 object Traits:
   trait Person
-  class Base(v: String) {
-    def this(arg1: Int) = this(arg1.toString)
+  class Base(val v: String) {
+    def this(arg1: Int) = this((arg1+1).toString)
     def this(arg2: Person) = this(arg2.toString)
     def this(arg3: Double) = this(arg3.toString)
   }
 
-  trait Extended extends Base
+  trait Extended extends Base:
+    override def toString: String = "Extended: " + v
 
   object Extended {
     def apply(arg1: Int) = new Base(arg1) with Extended
@@ -26,3 +27,4 @@ object Traits:
   }
   @main def runTraits(args: String*): Unit =
     println("File /Users/drmark/IdeaProjects/PLANE/src/main/scala/scala3Features/Traits.scala created at time 8:39PM")
+    println(Extended(1).toString)
