2ba76c3161db40f0afa8a9954e3fff2d55d2ac47
Mark Grechanik <markgrechanik@gmail.com>
2025-09-26 14:36:08 -0500

    parSequence example

---
+++ src/main/scala/CatsIO/Navigation/ParSequence.scala
@@ -0,0 +1,30 @@
+
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2025 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////
+
+package CatsIO.Navigation
+
+import CatsIO.Helpers.Aid4Debugging.debugInfo
+import CatsIO.Helpers.{bold, green}
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.*
+
+//sequence and traverse are mutually definable: x.sequence is
+//x.traverse(identity), and x.traverse(f) is x.map(f).sequence
+
+object ParSequence extends IOApp:
+  def log[T](message: String, instance: T): T =
+    println(message + ": " + instance.toString)
+    instance
+  end log
+
+  val tasks: List[IO[Int]] = List.tabulate(100)(task)
+  def task(id: Int): IO[Int] = IO(id).debugInfo()
+
+  def ParSequence_Program: IO[Unit] =
+    tasks.parSequence.debugInfo().map(r => log("Result: ".green.bold, r)).void
+
+  override def run(args: List[String]): IO[ExitCode] = ParSequence_Program.as(ExitCode.Success)
