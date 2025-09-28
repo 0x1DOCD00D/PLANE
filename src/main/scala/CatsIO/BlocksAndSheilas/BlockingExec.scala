987bb2b2078dd4590f5ea350c81e233d0d780cc2
Mark Grechanik <markgrechanik@gmail.com>
2025-09-28 18:09:02 -0500

    executors and thread factory

--- src/main/scala/CatsIO/BlocksAndSheilas/BlockingExec.scala
+++ src/main/scala/CatsIO/BlocksAndSheilas/BlockingExec.scala
@@ -10,28 +10,71 @@ package CatsIO.BlocksAndSheilas
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits.*
 import CatsIO.Helpers.Aid4Debugging.*
-import CatsIO.Helpers.{bold, green}
+import CatsIO.Helpers.{blue, bold, green, red}
+
 import scala.util.{Failure, Success, Try}
+import java.util.concurrent.{Executors, LinkedBlockingQueue, ScheduledExecutorService, ThreadFactory, ThreadPoolExecutor, TimeUnit}
+import scala.concurrent.ExecutionContext
+import cats.effect.unsafe.{IORuntime, IORuntimeConfig, Scheduler}
+
+def daemonThreadFactory(name: String): ThreadFactory =
+  new ThreadFactory {
+    private val default = Executors.defaultThreadFactory()
+    def newThread(r: Runnable): Thread = {
+      val t = default.newThread(r)
+      t.setName(name + "-" + t.getId)
+      t.setDaemon(true)
+      t
+    }
+  }
 
 object BlockingExec extends IOApp:
+  private val computeExecutor = Executors.newFixedThreadPool(8, daemonThreadFactory("compute"))
+  private val blockingExecutor = Executors.newFixedThreadPool(16, daemonThreadFactory("blocking"))
+  private val schedulerExecutor: ScheduledExecutorService =
+    Executors.newScheduledThreadPool(1, daemonThreadFactory("scheduler"))
+
+  // wrap into ExecutionContexts / Scheduler
+  private val computeEC: ExecutionContext = ExecutionContext.fromExecutorService(computeExecutor)
+  private val blockingEC: ExecutionContext = ExecutionContext.fromExecutorService(blockingExecutor)
+  private val scheduler: Scheduler = Scheduler.fromScheduledExecutor(schedulerExecutor)
+
+  // shutdown hook (called automatically when IOApp exits)
+  private val shutdown: () => Unit = () => {
+    computeExecutor.shutdown()
+    blockingExecutor.shutdown()
+    schedulerExecutor.shutdown()
+  }
+
+  // provide custom runtime
+  given IORuntime = IORuntime(
+    compute = computeEC,
+    blocking = blockingEC,
+    scheduler = scheduler,
+    shutdown = shutdown,
+    config = IORuntimeConfig()
+  )
   def log[T](message: String, instance: T): T =
     println(message + ": " + instance.toString)
     instance
   end log
 
   def BlockingExec_Program: IO[Unit] = for {
-    fib <- IO(println("/Users/drmark/IdeaProjects/PLANE/src/main/scala/CatsIO/BlocksAndSheilas/BlockingExec.scala")).start
-    _ <- IO.blocking("on blocker inside BlockingExec_Program").debugInfo()
+    _ <- IO.println(s"Step 1: ${Thread.currentThread.getName}".blue.bold).debugInfo()
+    _ <- IO.println(s"Step 2: ${Thread.currentThread.getName}".blue.bold).debugInfo()
+    fib <- IO(println("/Users/drmark/IdeaProjects/PLANE/src/main/scala/CatsIO/BlocksAndSheilas/BlockingExec.scala".red)).start
+    _ <- IO.blocking("on blocker inside BlockingExec_Program".green).debugInfo().evalOn(blockingEC)
+    _ <- IO.println("on default inside BlockingExec_Program").debugInfo().evalOn(computeEC)
     l1 = log("time: 9/28/25:", fib.toString)
     _ <- fib.join
   } yield ()
 
   def withBlocker: IO[Unit] =
       for {
-        _ <- IO.println("on default").debugInfo()
+        _ <- IO.println("on default").debugInfo().evalOn(computeEC)
         res <- IO.blocking {
-          println("on blocker inside withBlocker")
-        }.debugInfo()
+          println("on blocker inside withBlocker".green)
+        }.debugInfo().evalOn(blockingEC)
         _ <- IO.println("where am I?").debugInfo()
       } yield res
 
