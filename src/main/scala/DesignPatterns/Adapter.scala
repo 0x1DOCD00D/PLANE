cd7db7de7ce796a56b8ad3136b7300e1f6056291
Mark Grechanik <markgrechanik@gmail.com>
2025-11-20 15:55:30 -0600

    adapter pattern with extension

--- src/main/scala/DesignPatterns/Adapter.scala
+++ src/main/scala/DesignPatterns/Adapter.scala
@@ -1,12 +1,8 @@
-/*
- *
- *  * Copyright (c) 2020 Mark Grechanik. All rights reserved.
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
- *
- */
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2025 Mark Grechanik and Lone Star Consulting, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////
 
 package DesignPatterns
 
@@ -61,7 +57,11 @@ object AdapterPattern extends App {
     def NewMethod2 = adaptee.SomeCoreMethod(this.getClass.getPackageName)
   }
 
+  extension (adaptee: Adaptee)
+    def anotherMethod = adaptee.SomeCoreMethod(this.getClass.getName)
+
   //AutomaticAdapter((new Adaptee)).NewMethod
   (new Adaptee).NewMethod1
   (new Adaptee).NewMethod2
+  (new Adaptee).anotherMethod
 }
